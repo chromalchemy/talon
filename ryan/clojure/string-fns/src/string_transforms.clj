eb3d6524b6ad941ca993753ba78dd5cc9215414e
chromalchemy <ryanmarti@gmail.com>
2024-11-18 17:42:00 -0500

    remove-url-params bb fn

--- ryan/clojure/string-fns/src/string_transforms.clj
+++ ryan/clojure/string-fns/src/string_transforms.clj
@@ -1,9 +1,33 @@
 (ns string-transforms
   (:require [clojure.string :as string]))
 
+(+ 1 1)
+
 (defn stdin []
   (slurp *in*))
 
-(defn uppercase [x]
-  (println 
-    (string/upper-case (stdin))))
+(defn run-on-stdin [f]
+  (println
+    (f (stdin))))
+
+(def uppercase string/upper-case)
+
+(defn uppercase! [x]
+  (run-on-stdin uppercase))
+
+(def url-w-params* "https://getbitoutdoors.com/slab-sniper-76/?showHidden=true&ctk=d59bf5f4-44ae-418b-acbb-6ce04dc8990d")
+
+(comment 
+  (uppercase "kdjfoefji"))
+
+(defn remove-url-params [s]
+  (-> s
+    #_(string/replace "?" "")
+    (string/split #"\?")
+    (first)))
+
+(defn remove-url-params! [x]
+  (run-on-stdin remove-url-params))
+
+(comment 
+  (remove-url-params url-w-params*))
