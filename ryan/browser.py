6e69502e49fa842a77ef427dd1ea133a9f7166c3
chromalchemy <ryanmarti@gmail.com>
2025-06-06 12:19:28 -0400

    start custome browser.py

--- ryan/browser.py
+++ ryan/browser.py
@@ -1,9 +1,9 @@
-from talon import Module, actions
+# from talon import Module, actions
 
-mod = Module()
+# mod = Module()
 
-@mod.action_class
-class Actions:
+# @mod.action_class
+# class Actions:
     # def rango_click_mark(mark: str):
     #     """click on a named rango mark"""
     #     actions.user.rango_run_action_on_reference("clickElement", mark)
