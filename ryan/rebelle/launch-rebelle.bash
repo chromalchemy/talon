956a3a3cf79335e389d05db52e871e7938b56bca
chromalchemy <ryanmarti@gmail.com>
2025-07-23 00:38:38 -0400

    launch rebelle app w flags via bb task

--- ryan/rebelle/launch-rebelle.bash
+++ ryan/rebelle/launch-rebelle.bash
@@ -1,3 +1,3 @@
 
 # Launch Rebelle with WebSocket server enabled:
-"/Applications/Rebelle 7.app/Contents/MacOS/Rebelle 7" -websocket-server-enable -websocket-port 8265 -websocket-allowed-ip-addresses 172.20.10.7,127.0.0.1
+"/Applications/Rebelle 8.app/Contents/MacOS/Rebelle 7" -websocket-server-enable -websocket-port 8265 -websocket-allowed-ip-addresses 172.20.10.7,127.0.0.1
