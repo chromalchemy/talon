84495f4815c578c728ed5ca9e0de13183c74c996
chromalchemy <ryanmarti@gmail.com>
2025-07-23 12:05:57 -0400

    move rebelle clj stuff to it's own repo

--- ryan/rebelle/launch-rebelle.bash
+++
@@ -1,3 +0,0 @@
-
-# Launch Rebelle with WebSocket server enabled:
-"/Applications/Rebelle 8.app/Contents/MacOS/Rebelle 7" -websocket-server-enable -websocket-port 8265 -websocket-allowed-ip-addresses 172.20.10.7,127.0.0.1
