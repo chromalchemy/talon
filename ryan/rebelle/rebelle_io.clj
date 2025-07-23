19c5f25f2f5e82335efa2dbc1b1edb8ace84c3ce
chromalchemy <ryanmarti@gmail.com>
2025-07-23 11:00:39 -0400

    Rebelle: start to work with clojure edn  event maps

--- ryan/rebelle/rebelle_io.clj
+++ ryan/rebelle/rebelle_io.clj
@@ -5,6 +5,22 @@
    [clojure.java.io :as io]
    [cheshire.core :as json]))
 
+(def default-brush
+  {:size 100
+   :opacity 100
+   :paint_type "PAINT"
+  ;;  :glaze_mode "OPAQUE"
+   #_#_:water 55
+   :color {:r 0 :g 0 :b 0}
+   :tool       "WATERCOLOR"
+   :preset     "Gouache/Gouache Filbert"})
+
+(defn set-brush! 
+  [{:keys [] :as m}]
+  (merge 
+    {:event_type "SET_BRUSH"}
+    m))
+
 (defn get-test-event
   "Reads and parses the test_event.json file."
   []
@@ -14,10 +30,15 @@
 (defn send-rebelle-command
   "Sends a command to the Rebelle WebSocket server using websocat and babashka.process."
   [command-map]
-  (let [command-json (str (json/encode command-map) "\n")]
+  (let [command-json 
+        (str (json/encode command-map) "\n")]
     (try
-      (let [process (p/process ["websocat" "ws://localhost:8265"]
-                               {:in command-json :out :stream :err :stream})
+      (let [process
+            (p/process
+              ["websocat" "ws://localhost:8265"]
+              {:in command-json 
+               :out :stream 
+               :err :stream})
             out (slurp (:out process))
             err (slurp (:err process))]
         (p/check process)
@@ -27,5 +48,9 @@
       (catch Exception e
         (println "❌ Error sending command:" (ex-message e))))))
 
+
+
+
+
 (comment
-  (send-rebelle-command (get-test-event)))
+  (send-rebelle-command (set-brush! default-brush)))
