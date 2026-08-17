4c890d11ed0008cf195ecd3b803452e9ef3b1c69
chromalchemy <ryanmarti@gmail.com>
2026-08-16 22:04:12 -0400

    longer context menu nav timeouts (buggy in vscode)

--- ryan/system_nav/context_menu.py
+++ ryan/system_nav/context_menu.py
@@ -8,10 +8,11 @@ class Actions:
         """Run Command from context menu"""
         actions.tracking.zoom_cancel()
         actions.mouse_click(1)
-        actions.sleep("200ms")
+        actions.sleep("300ms")
+        # actions.key("down")
         actions.insert(cmd_str)
-        actions.sleep("200ms")
+        actions.sleep("500ms")
         actions.key("enter")
-        # close the mouse grid if open
+        # # close the mouse grid if open
         actions.user.grid_close()
         
